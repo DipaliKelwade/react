39d6898bb998d7f69c043800045b8a12026aa1c8
DipaliKelwade <dipalikelwade595@gmail.com>
2024-02-07 21:27:35 +0530

    keeper-app

---
+++ keeper-app/src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Footer() {
+  const name = "Dipali Kelwade";
+  return (
+  
+      <footer>
+        <p> Copyright © {name}</p>
+      </footer>
+  
+  );
+}
+
+export default Footer;
